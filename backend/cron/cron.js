0a97152670aaba3c2afcaaf7d4035292a2321122
Devendra Lohar <devlo@Devendras-Laptop.local>
2024-12-20 23:13:51 +0900

    Cron Job for activity

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://example.com";
+const URL = "https://cognite-chat-app.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
